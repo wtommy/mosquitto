af89ec66b9a2078d48a7543ea153f77e8a06c130
Roger Light <roger@atchoo.org>
2009-10-26 07:59:49 +0000

    Future library header.

---
+++ library/mqtt3.h
@@ -0,0 +1,23 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+/* For version 3 of the MQTT protocol */
+
+/* Message types */
+#define CONNECT 1
+#define CONNACK 2
+#define PUBLISH 3
+#define PUBACK 4
+#define PUBREC 5
+#define PUBREL 6
+#define PUBCOMP 7
+#define SUBSCRIBE 8
+#define SUBACK 9
+#define UNSUBSCRIBE 10
+#define UNSUBACK 11
+#define PINGREG 12
+#define PINGRESP 13
+#define DISCONNECT 14
+
+
+#endif
