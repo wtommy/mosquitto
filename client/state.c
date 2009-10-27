0363b6c0307cdcc2c3f652c1ea6f2e828f1dbee4
Roger Light <roger@atchoo.org>
2009-10-27 18:13:13 +0000

    Function for handling PUBLISH events (QoS 0 only).

--- client/state.c
+++ client/state.c
@@ -22,6 +22,30 @@ typedef enum {
 
 static stateType state = stStart;
 
+void mqtt_handle_publish(int sock, uint8_t header)
+{
+	uint8_t *topic, *payload;
+	uint8_t remaining_length;
+	uint8_t dup, qos, retain;
+
+	dup = (header & 0x08)>>3;
+	qos = (header & 0x06)>>1;
+	retain = (header & 0x01);
+
+	remaining_length = mqtt_read_byte(sock);
+	//read(sock, &byte, 1); // Remaining length (should be 12)
+
+	topic = mqtt_read_string(sock);
+	remaining_length -= strlen((char *)topic) - 2;
+	printf("Topic: %s\n", topic);
+	free(topic);
+
+	payload = malloc(remaining_length*sizeof(uint8_t));
+	mqtt_read_bytes(sock, payload, remaining_length);
+	printf("Payload: %s\n", payload);
+	free(payload);
+}
+
 void handle_read(int sock)
 {
 	uint8_t buf;
@@ -52,35 +76,10 @@ void handle_read(int sock)
 			break;
 		case PUBLISH:
 			printf("Received PUBLISH\n");
-			read(sock, &buf, 1); // Remaining length (should be 12)
-			printf("%d ", buf);
-			read(sock, &buf, 1); // Topic MSB
-			printf("%d ", buf);
-			read(sock, &buf, 1); // Topic LSB
-			printf("%d ", buf);
-			read(sock, &buf, 1); // Topic
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c ", buf);
-			read(sock, &buf, 1); // Payload
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c", buf);
-			read(sock, &buf, 1);
-			printf("%c\n", buf);
+			mqtt_handle_publish(sock, buf);
 			break;
 		default:
-			printf("Unknown command: %s\n", mqtt_command_to_string(buf&0xF0));
+			printf("Unknown command: %s (%d)\n", mqtt_command_to_string(buf&0xF0), buf&0xF0);
 			break;
 	}
 }
@@ -125,7 +124,7 @@ int main(int argc, char *argv[])
 					break;
 				case stConnAckd:
 					printf("CONNACK received\n");
-					mqtt_raw_subscribe(sock, false, "a/b/c", 5, 0);
+					mqtt_raw_subscribe(sock, false, "$SYS/broker/log", 15, 0);
 					state = stSubSent;
 					break;
 				case stSubSent:
