7798809c6ed51205c75867be81c6e2d019162b4b
Roger Light <roger@atchoo.org>
2010-03-11 08:36:23 +0000

    Variable should be here, but only for WITH_REGEX.

--- src/database.c
+++ src/database.c
@@ -143,6 +143,9 @@ int (*client_publish_callback)(const char *, int, uint32_t, const uint8_t *, int
 
 int mqtt3_db_open(mqtt3_config *config)
 {
+#ifdef WITH_REGEX
+	char *errmsg = NULL;
+#endif
 	int dbrc;
 	int rc = 0;
 	sqlite3_backup *restore;
