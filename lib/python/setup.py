757c0d79470428607b3d183d644cdf67577ce793
Roger Light <roger@atchoo.org>
2010-07-21 23:56:43 +0100

    Add python setup script.

---
+++ lib/python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='mosquitto',
+	version='0.8pre',
+	description='MQTT version 3 client class',
+	author='Roger Light',
+	author_email='roger@atchoo.org',
+	url='http://mosquitto.org/',
+	download_url='http://mosquitto.org/files/',
+	license='BSD License',
+	py_modules=['mosquitto']
+	)
