d8a97ede125f667bf1f0687defae75fa101ba304
Roger Light <roger@atchoo.org>
2010-09-14 11:21:23 +0100

    Use dedicated function to tokenise subs/topics before doing anything so that nesting won't break anything.

--- src/subs.c
+++ src/subs.c
@@ -34,18 +34,65 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <subs.h>
 #include <memory_mosq.h>
 
-static int _sub_add(mqtt3_context *context, int qos, struct _mosquitto_subhier *subhier, char *sub)
+struct _sub_token {
+	struct _sub_token *next;
+	char *topic;
+};
+
+static int _sub_topic_tokenise(const char *subtopic, struct _sub_token **topics)
 {
+	struct _sub_token *new_topic, *tail = NULL;
 	char *token;
+	char *local_subtopic = NULL;
+
+	assert(subtopic);
+	assert(topics);
+
+	local_subtopic = _mosquitto_strdup(subtopic);
+	if(!local_subtopic) return 1;
+
+	token = strtok(local_subtopic, "/");
+	while(token){
+		new_topic = _mosquitto_malloc(sizeof(struct _sub_token));
+		if(!new_topic) goto cleanup;
+		new_topic->next = NULL;
+		new_topic->topic = _mosquitto_strdup(token);
+		if(!new_topic->topic) goto cleanup;
+
+		if(tail){
+			tail->next = new_topic;
+			tail = tail->next;
+		}else{
+			tail = new_topic;
+			*topics = tail;
+		}
+		token = strtok(NULL, "/");
+	}
+	
+	_mosquitto_free(local_subtopic);
+
+	return 0;
+
+cleanup:
+	_mosquitto_free(local_subtopic);
+
+	tail = *topics;
+	*topics = NULL;
+	while(tail){
+		if(tail->topic) _mosquitto_free(tail->topic);
+		new_topic = tail->next;
+		_mosquitto_free(tail);
+		tail = new_topic;
+	}
+	return 1;
+}
+
+static int _sub_add(mqtt3_context *context, int qos, struct _mosquitto_subhier *subhier, struct _sub_token *tokens)
+{
 	struct _mosquitto_subhier *branch, *last = NULL;
 	struct _mosquitto_subleaf *leaf, *last_leaf;
 
-	if(!sub){
-		token = strtok(NULL, "/");
-	}else{
-		token = strtok(sub, "/");
-	}
-	if(!token){
+	if(!tokens){
 		leaf = subhier->subs;
 		last_leaf = NULL;
 		while(leaf){
@@ -72,8 +119,8 @@ static int _sub_add(mqtt3_context *context, int qos, struct _mosquitto_subhier *
 
 	branch = subhier->children;
 	while(branch){
-		if(!strcmp(branch->topic, token)){
-			return _sub_add(context, qos, branch, NULL);
+		if(!strcmp(branch->topic, tokens->topic)){
+			return _sub_add(context, qos, branch, tokens->next);
 		}
 		last = branch;
 		branch = branch->next;
@@ -86,22 +133,16 @@ static int _sub_add(mqtt3_context *context, int qos, struct _mosquitto_subhier *
 	}else{
 		last->next = branch;
 	}
-	branch->topic = _mosquitto_strdup(token);
-	return _sub_add(context, qos, branch, NULL);
+	branch->topic = _mosquitto_strdup(tokens->topic);
+	return _sub_add(context, qos, branch, tokens->next);
 }
 
-static int _sub_remove(mqtt3_context *context, struct _mosquitto_subhier *subhier, char *sub)
+static int _sub_remove(mqtt3_context *context, struct _mosquitto_subhier *subhier, struct _sub_token *tokens)
 {
-	char *token;
 	struct _mosquitto_subhier *branch, *last = NULL;
 	struct _mosquitto_subleaf *leaf, *last_leaf;
 
-	if(!sub){
-		token = strtok(NULL, "/");
-	}else{
-		token = strtok(sub, "/");
-	}
-	if(!token){
+	if(!tokens){
 		leaf = subhier->subs;
 		last_leaf = NULL;
 		while(leaf){
@@ -123,8 +164,8 @@ static int _sub_remove(mqtt3_context *context, struct _mosquitto_subhier *subhie
 
 	branch = subhier->children;
 	while(branch){
-		if(!strcmp(branch->topic, token)){
-			_sub_remove(context, branch, NULL);
+		if(!strcmp(branch->topic, tokens->topic)){
+			_sub_remove(context, branch, tokens->next);
 			if(!branch->children && !branch->subs){
 				if(last){
 					last->next = branch->next;
@@ -142,18 +183,12 @@ static int _sub_remove(mqtt3_context *context, struct _mosquitto_subhier *subhie
 	return 0;
 }
 
-static int _sub_search(struct _mosquitto_subhier *subhier, char *topic)
+static int _sub_search(struct _mosquitto_subhier *subhier, struct _sub_token *tokens)
 {
-	char *token;
 	struct _mosquitto_subhier *branch, *last = NULL;
 	struct _mosquitto_subleaf *leaf, *last_leaf;
 
-	if(!topic){
-		token = strtok(NULL, "/");
-	}else{
-		token = strtok(topic, "/");
-	}
-	if(!token){
+	if(!tokens){
 		leaf = subhier->subs;
 		last_leaf = NULL;
 		while(leaf){
@@ -165,10 +200,10 @@ static int _sub_search(struct _mosquitto_subhier *subhier, char *topic)
 
 	branch = subhier->children;
 	while(branch){
-		if(!strcmp(branch->topic, token) || !strcmp(branch->topic, "+")){
+		if(!strcmp(branch->topic, tokens->topic) || !strcmp(branch->topic, "+")){
 			/* The topic matches this subscription.
 			 * Doesn't include # wildcards */
-			_sub_search(branch, NULL);
+			_sub_search(branch, tokens->next);
 		}else if(!strcmp(branch->topic, "#") && !branch->children){
 			/* The topic matches due to a # wildcard - process the
 			 * subscriptions and return. */
@@ -183,145 +218,135 @@ static int _sub_search(struct _mosquitto_subhier *subhier, char *topic)
 
 int mqtt3_sub_add(mqtt3_context *context, int qos, struct _mosquitto_subhier *root, const char *sub)
 {
-	char *local_sub;
 	int tree;
+	int rc = 0;
 	struct _mosquitto_subhier *subhier;
-	int rc;
+	struct _sub_token *tokens = NULL, *tail;
 
 	assert(root);
 	assert(sub);
 
 	if(!strncmp(sub, "$SYS", 4)){
 		tree = 2;
-		local_sub = _mosquitto_strdup(sub);
+		if(_sub_topic_tokenise(sub, &tokens)) return 1;
 	}else if(sub[0] == '/'){
 		tree = 1;
-		local_sub = _mosquitto_strdup(sub+1);
+		if(_sub_topic_tokenise(sub+1, &tokens)) return 1;
 	}else{
 		tree = 0;
-		local_sub = _mosquitto_strdup(sub);
+		if(_sub_topic_tokenise(sub, &tokens)) return 1;
 	}
-	if(!local_sub) return 1;
 
 	subhier = root->children;
 	while(subhier){
 		if(!strcmp(subhier->topic, "") && tree == 0){
-			rc = _sub_add(context, qos, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_add(context, qos, subhier, tokens);
+			break;
 		}else if(!strcmp(subhier->topic, "/") && tree == 1){
-			rc = _sub_add(context, qos, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_add(context, qos, subhier, tokens);
+			break;
 		}else if(!strcmp(subhier->topic, "$SYS") && tree == 2){
-			rc = _sub_add(context, qos, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_add(context, qos, subhier, tokens);
+			break;
 		}
 		subhier = subhier->next;
 	}
 
-	_mosquitto_free(local_sub);
-	return 1;
+	while(tokens){
+		tail = tokens->next;
+		_mosquitto_free(tokens->topic);
+		_mosquitto_free(tokens);
+		tokens = tail;
+	}
+	return rc;
 }
 
 int mqtt3_sub_remove(mqtt3_context *context, struct _mosquitto_subhier *root, const char *sub)
 {
-	char *local_sub;
+	int rc = 0;
 	int tree;
 	struct _mosquitto_subhier *subhier;
-	int rc;
+	struct _sub_token *tokens = NULL, *tail;
 
 	assert(root);
 	assert(sub);
 
 	if(!strncmp(sub, "$SYS", 4)){
 		tree = 2;
-		local_sub = _mosquitto_strdup(sub);
+		if(_sub_topic_tokenise(sub, &tokens)) return 1;
 	}else if(sub[0] == '/'){
 		tree = 1;
-		local_sub = _mosquitto_strdup(sub+1);
+		if(_sub_topic_tokenise(sub, &tokens)) return 1;
 	}else{
 		tree = 0;
-		local_sub = _mosquitto_strdup(sub);
+		if(_sub_topic_tokenise(sub, &tokens)) return 1;
 	}
-	if(!local_sub) return 1;
 
 	subhier = root->children;
 	while(subhier){
 		if(!strcmp(subhier->topic, "") && tree == 0){
-			rc = _sub_remove(context, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_remove(context, subhier, tokens);
+			break;
 		}else if(!strcmp(subhier->topic, "/") && tree == 1){
-			rc = _sub_remove(context, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_remove(context, subhier, tokens);
+			break;
 		}else if(!strcmp(subhier->topic, "$SYS") && tree == 2){
-			rc = _sub_remove(context, subhier, local_sub);
-			_mosquitto_free(local_sub);
-			return rc;
+			rc = _sub_remove(context, subhier, tokens);
+			break;
 		}
 		subhier = subhier->next;
 	}
 
-	_mosquitto_free(local_sub);
-	return 1;
+	while(tokens){
+		tail = tokens->next;
+		_mosquitto_free(tokens->topic);
+		_mosquitto_free(tokens);
+		tokens = tail;
+	}
+
+	return rc;
 }
 
 int mqtt3_sub_search(struct _mosquitto_subhier *root, const char *topic)
 {
-	char *stmp;
-	int hier;
-	char *local_topic;
+	int rc = 0;
 	int tree;
 	struct _mosquitto_subhier *subhier;
+	struct _sub_token *tokens = NULL, *tail;
 
 	assert(root);
 	assert(topic);
 
 	if(!strncmp(topic, "$SYS", 4)){
 		tree = 2;
-		local_topic = _mosquitto_strdup(topic);
+		if(_sub_topic_tokenise(topic, &tokens)) return 1;
 	}else if(topic[0] == '/'){
 		tree = 1;
-		local_topic = _mosquitto_strdup(topic+1);
+		if(_sub_topic_tokenise(topic, &tokens)) return 1;
 	}else{
 		tree = 0;
-		local_topic = _mosquitto_strdup(topic);
-	}
-	if(!local_topic) return 1;
-
-	hier = 0;
-	stmp = local_topic;
-	while(stmp){
-		stmp = index(stmp, '/');
-		if(stmp) stmp++;
-		hier++;
+		if(_sub_topic_tokenise(topic, &tokens)) return 1;
 	}
 
 	subhier = root->children;
 	while(subhier){
 		if(!strcmp(subhier->topic, "") && tree == 0){
-			if(_sub_search(subhier, local_topic)){
-				_mosquitto_free(local_topic);
-				return 1;
-			}
+			rc = _sub_search(subhier, tokens);
 		}else if(!strcmp(subhier->topic, "/") && tree == 1){
-			if(_sub_search(subhier, local_topic)){
-				_mosquitto_free(local_topic);
-				return 1;
-			}
+			rc = _sub_search(subhier, tokens);
 		}else if(!strcmp(subhier->topic, "$SYS") && tree == 2){
-			if(_sub_search(subhier, local_topic)){
-				_mosquitto_free(local_topic);
-				return 1;
-			}
+			rc = _sub_search(subhier, tokens);
 		}
 		subhier = subhier->next;
 	}
-	_mosquitto_free(local_topic);
-	return 0;
+	while(tokens){
+		tail = tokens->next;
+		_mosquitto_free(tokens->topic);
+		_mosquitto_free(tokens);
+		tokens = tail;
+	}
+
+	return rc;
 }
 
 void mqtt3_sub_tree_print(struct _mosquitto_subhier *root, int level)
