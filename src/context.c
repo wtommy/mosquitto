543a216a775bb15f1fdd9e5da8e22ca5d18e0088
Roger Light <roger@atchoo.org>
2010-09-18 23:55:38 +0100

    Print client connections from ipv6 correctly.

--- src/context.c
+++ src/context.c
@@ -37,7 +37,7 @@ POSSIBILITY OF SUCH DAMAGE.
 mqtt3_context *mqtt3_context_init(int sock)
 {
 	mqtt3_context *context;
-	struct sockaddr addr;
+	struct sockaddr_storage addr;
 	socklen_t addrlen;
 	char address[1024];
 
@@ -59,9 +59,15 @@ mqtt3_context *mqtt3_context_init(int sock)
 
 	addrlen = sizeof(addr);
 	context->address = NULL;
-	if(!getpeername(sock, &addr, &addrlen)){
-		if(inet_ntop(AF_INET, &((struct sockaddr_in *)&addr)->sin_addr.s_addr, address, 1024)){
-			context->address = _mosquitto_strdup(address);
+	if(!getpeername(sock, (struct sockaddr *)&addr, &addrlen)){
+		if(addr.ss_family == AF_INET){
+			if(inet_ntop(AF_INET, &((struct sockaddr_in *)&addr)->sin_addr.s_addr, address, 1024)){
+				context->address = _mosquitto_strdup(address);
+			}
+		}else if(addr.ss_family == AF_INET6){
+			if(inet_ntop(AF_INET6, &((struct sockaddr_in6 *)&addr)->sin6_addr.s6_addr, address, 1024)){
+				context->address = _mosquitto_strdup(address);
+			}
 		}
 	}
 	context->bridge = NULL;
