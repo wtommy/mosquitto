5e6a610cd7cedadd353ee23d7f2ba92d512682ed
Roger Light <roger@atchoo.org>
2010-11-02 09:42:11 +0000

    Free client_id and topic after sub restore.

--- src/persist.c
+++ src/persist.c
@@ -361,6 +361,7 @@ static int _db_sub_chunk_restore(mosquitto_db *db, int db_fd)
 	uint8_t qos;
 	char *client_id;
 	char *topic;
+	int rc = 0;
 
 #define read_e(a, b, c) if(read(a, b, c) != c){ goto error; }
 	read_e(db_fd, &i16temp, sizeof(uint16_t));
@@ -378,16 +379,19 @@ static int _db_sub_chunk_restore(mosquitto_db *db, int db_fd)
 	if(!topic){
 		close(db_fd);
 		mqtt3_log_printf(MOSQ_LOG_ERR, "Error: Out of memory.");
+		_mosquitto_free(client_id);
 		return 1;
 	}
 	read_e(db_fd, &topic, slen);
 	read_e(db_fd, &qos, sizeof(uint8_t));
 	if(_db_restore_sub(db, client_id, topic, qos)){
-		return 1;
+		rc = 1;
 	}
 #undef read_e
+	_mosquitto_free(client_id);
+	_mosquitto_free(topic);
 
-	return 0;
+	return rc;
 error:
 	mqtt3_log_printf(MOSQ_LOG_ERR, "Error: %s.", strerror(errno));
 	if(db_fd >= 0) close(db_fd);
