da19ed86f2ee299b19adcc317b63f22024459cdc
Roger Light <roger@atchoo.org>
2011-04-25 21:48:35 +0100

    Update copyright years on modified files.

--- lib/mosquitto_internal.h
+++ lib/mosquitto_internal.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010, Roger Light <roger@atchoo.org>
+Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
