aec85898ad0088438297f8bbc499c9c11713372a
Roger Light <roger@atchoo.org>
2011-05-05 12:51:36 +0100

    Share PUBLISH sending code with library and broker.
    --HG--
    branch : 0.11

--- lib/send_mosq.c
+++ lib/send_mosq.c
@@ -31,12 +31,11 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 
 #include <mosquitto.h>
+#include <mosquitto_internal.h>
 #include <logging_mosq.h>
-#include <memory_mosq.h>
 #include <mqtt3_protocol.h>
 #include <net_mosq.h>
 #include <send_mosq.h>
-#include <util_mosq.h>
 
 int _mosquitto_send_pingreq(struct mosquitto *mosq)
 {
@@ -65,47 +64,12 @@ int _mosquitto_send_pubcomp(struct mosquitto *mosq, uint16_t mid)
 
 int _mosquitto_send_publish(struct mosquitto *mosq, uint16_t mid, const char *topic, uint32_t payloadlen, const uint8_t *payload, int qos, bool retain, bool dup)
 {
-	struct _mosquitto_packet *packet = NULL;
-	int packetlen;
-	int rc;
-
 	assert(mosq);
 	assert(topic);
 
 	if(mosq->core.sock == INVALID_SOCKET) return MOSQ_ERR_NO_CONN;
-
 	if(mosq) _mosquitto_log_printf(mosq, MOSQ_LOG_DEBUG, "Sending PUBLISH (d%d, q%d, r%d, m%d, '%s', ... (%ld bytes))", dup, qos, retain, mid, topic, (long)payloadlen);
-
-	packetlen = 2+strlen(topic) + payloadlen;
-	if(qos > 0) packetlen += 2; /* For message id */
-	packet = _mosquitto_calloc(1, sizeof(struct _mosquitto_packet));
-	if(!packet){
-		_mosquitto_log_printf(mosq, MOSQ_LOG_DEBUG, "PUBLISH failed allocating packet memory.");
-		return MOSQ_ERR_NOMEM;
-	}
-
-	packet->mid = mid;
-	packet->command = PUBLISH | ((dup&0x1)<<3) | (qos<<1) | retain;
-	packet->remaining_length = packetlen;
-	rc = _mosquitto_packet_alloc(packet);
-	if(rc){
-		_mosquitto_free(packet);
-		return rc;
-	}
-	/* Variable header (topic string) */
-	_mosquitto_write_string(packet, topic, strlen(topic));
-	if(qos > 0){
-		_mosquitto_write_uint16(packet, mid);
-	}
-
-	/* Payload */
-	if(payloadlen){
-		_mosquitto_write_bytes(packet, payload, payloadlen);
-	}
-
-	_mosquitto_packet_queue(&mosq->core, packet);
-
-	return MOSQ_ERR_SUCCESS;
+	return _mosquitto_send_real_publish(&mosq->core, mid, topic, payloadlen, payload, qos, retain, dup);
 }
 
 int _mosquitto_send_pubrec(struct mosquitto *mosq, uint16_t mid)
