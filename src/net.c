1c0972313fa9235feffb2987bb83816bf00e1509
Roger Light <roger@atchoo.org>
2011-07-15 16:33:38 +0100

    Update copyrights.
    --HG--
    branch : 0.12

--- src/net.c
+++ src/net.c
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2009-2011 Roger Light <roger@atchoo.org>
+Copyright (c) 2011 Yuvraaj Kelkar <yuvraaj@gmail.com>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
--- src/subs.c
+++ src/subs.c
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2010,2011 Roger Light <roger@atchoo.org>
+Copyright (c) 2011 Sang Kyeong Nam
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
