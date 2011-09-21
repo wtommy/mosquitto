64315de1510409b867d497e74571be3337e4ec88
Roger Light <roger@atchoo.org>
2011-09-21 23:08:53 +0100

    Bump version number for 0.13.90.
    --HG--
    branch : 0.14

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='mosquitto',
-	version='0.13',
+	version='0.13.90',
 	description='MQTT version 3.1 client class',
 	author='Roger Light',
 	author_email='roger@atchoo.org',
