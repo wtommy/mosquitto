1129c772fb420459eae068ef8e44079510d9afd6
Roger Light <roger@atchoo.org>
2012-01-29 22:10:43 +0000

    Bump copyright dates.
    --HG--
    branch : 0.15

--- src/conf.c
+++ src/conf.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2011 Roger Light <roger@atchoo.org>
+Copyright (c) 2009-2012 Roger Light <roger@atchoo.org>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
